fb17e896b5fcf2c12e54bc2ff1f07a18cededa92
Iqbal Fauzan <iqbalfauzan@Iqbals-Mac-mini.local>
2022-10-14 19:15:04 +0700

    first commit

---
+++ app/src/main/java/net/iqbalfauzan/androidjetpackcompose/MainActivity.kt
@@ -0,0 +1,89 @@
+package net.iqbalfauzan.androidjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import net.iqbalfauzan.androidjetpackcompose.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            Greeting(name = "Iqbal!")
+//            BirthdayGreetingWithText(message = "Hello World,", from = "I'm Iqbal Using Compose")
+            HappyBirthdayTheme {
+                // A surface container that uses the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    BirthdayCardPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello World, I'm $name Using Compose", style = TextStyle(color = Color.Red))
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage( "Happy Birthday Iqbal!", "- from Ayang Eyin")
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    // Create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
